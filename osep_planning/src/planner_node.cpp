a8b810e478870fc016a26a641703ba8017da0a45
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-09-09 16:24:12 +0200

    new points

--- osep_planning/src/planner_node.cpp
+++ osep_planning/src/planner_node.cpp
@@ -209,9 +209,9 @@ PlannerNode::PlannerNode() : Node("PlannerNode") {
     for (int i=0; i<2; ++i) {
         bootstrap_waypoints_[i].pose.orientation.w = 1.0; // identity
     }
-    bootstrap_waypoints_[0].pose.position.x = 0.0;
+    bootstrap_waypoints_[0].pose.position.x = 160.0;
     bootstrap_waypoints_[0].pose.position.y = 0.0;
-    bootstrap_waypoints_[0].pose.position.z = 80.0;
+    bootstrap_waypoints_[0].pose.position.z = 120.0;
 
     bootstrap_waypoints_[1].pose.position.x = 180.0;
     bootstrap_waypoints_[1].pose.position.y = 0.0;
